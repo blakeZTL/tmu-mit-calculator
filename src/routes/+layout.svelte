051c8c3063ea2b12c36a5ce4357b3d63c95dff53
Blake Bradford <james.b.bradford@faa.gov>
2024-07-07 11:29:46 -0400

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
+	import '../app.css';
+	import { faEllipsis } from '@fortawesome/free-solid-svg-icons';
+</script>
+
+<div class="navbar bg-base-100">
+	<div class="flex-1">
+		<a href="/" class="btn btn-ghost text-xl">MIT Calculator</a>
+	</div>
+	<div class="flex-none">
+		<button class="btn btn-square btn-ghost">
+			<FontAwesomeIcon icon={faEllipsis} size="2x" />
+		</button>
+	</div>
+</div>
+<slot />
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+import daisyui from 'daisyui';
+
+module.exports = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {}
+	},
+	plugins: [daisyui]
+};
